820823078ca845bd852b4c64e1034ee7fb470cf8
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-29 22:17:40 +0700

    feat: enhance Navbar layout with responsive Menu button and background styling

--- apps/web/src/Components/Navbar/Navbar.tsx
+++ apps/web/src/Components/Navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { DM_Sans } from "next/font/google";
+
+import { Button } from "../ui/button";
 import LocaleSelector from "./LocaleSelector";
 
 const dmSans = DM_Sans({ subsets: ["latin"], weight: ["400"] });
@@ -8,7 +10,10 @@ const Navbar = () => {
 	return (
 		<nav className="absolute top-0 z-50 w-full flex flex-col items-center py-6 px-4 md:px-10">
 			{/* menu bar centered */}
-			<div className="flex items-center gap-4 bg-[#454849]/20 py-2 px-2 rounded-full mx-auto">
+			<div
+				className="w-full lg:w-fit flex items-center justify-between lg:justify-start gap-4 
+          py-2 px-2 rounded-full mx-auto bg-[#40404033]/80"
+			>
 				<div className="flex items-center gap-2">
 					<Image src={"/Union.svg"} alt="Logo" width={40} height={40} />
 				</div>
@@ -20,6 +25,14 @@ const Navbar = () => {
 					<li className="hover:text-white cursor-pointer">Why Merano</li>
 					<li className="hover:text-white cursor-pointer">About Us</li>
 				</ul>
+
+				<Button
+					className="block lg:hidden text-white"
+					type="button"
+					variant={"ghost"}
+				>
+					Menu
+				</Button>
 			</div>
 
 			<div className="relative mt-2 md:absolute hidden lg:block md:right-10">
