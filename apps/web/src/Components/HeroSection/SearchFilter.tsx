4746d4a1a18a2a541fcc5bb6febc90b0de556201
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-29 17:45:21 +0700

    feat: refactor SearchFilter component to use buttons for date and person selection

--- apps/web/src/Components/HeroSection/SearchFilter.tsx
+++ apps/web/src/Components/HeroSection/SearchFilter.tsx
@@ -1,44 +1,47 @@
 import { Search, CalendarDays, UserRound } from "lucide-react";
 import { Label } from "../ui/label";
+import { Button } from "../ui/button";
 
 const SearchFilter = () => {
 	return (
 		<div className="w-full max-w-2xl mx-auto rounded-2xl shadow-lg mt-24">
 			{/* Labels grid aligned with inputs */}
-			<div className="grid grid-cols-[1fr_1px_1fr_56px] px-1 mb-1">
+			<div className="grid grid-cols-[1fr_1px_1fr_3.5rem] px-1 mb-1">
 				<Label className="text-white font-light ml-0.5">
 					Arrival & Departure
 				</Label>
 				<Label className="text-white font-light ml-0.5">Person</Label>
 			</div>
 			<div className="rounded-2xl bg-gray-300/10 p-1.5 overflow-hidden backdrop-blur-xs">
-				<div className="grid grid-cols-[1fr_1px_1fr_56px] overflow-visible">
+				<div className="grid grid-cols-[1fr_1px_1fr_3.5rem] overflow-visible">
 					{/* Inputs */}
-					<div className="flex items-center gap-2 flex-1 px-4 py-3 bg-white/90 rounded-l-2xl border-r border-gray-400">
-						<CalendarDays className="w-6 h-6 text-gray-500" />
-						<input
-							type="text"
-							placeholder="Select Date"
-							className="bg-transparent outline-none text-gray-700 w-full placeholder:text-gray-400"
-						/>
-					</div>
+					<button
+						aria-label="Select Date"
+						type="button"
+						className="flex items-center gap-2 flex-1 px-4 py-3 bg-white/90 rounded-l-2xl border-r border-gray-400 cursor-pointer"
+					>
+						<CalendarDays className="w-6 h-6 text-[#18191A]" />
+						<p className="text-[#707577] font-light">Select Date</p>
+					</button>
 					{/* Divider */}
 					<div className="w-px my-2" />
-					<div className="flex items-center gap-2 flex-1 px-4 py-3 bg-white/90">
-						<UserRound className="w-6 h-6 text-gray-500" />
-						<input
-							type="text"
-							placeholder="0 Person"
-							className="bg-transparent outline-none text-gray-700 w-full placeholder:text-gray-400 border-l border-gray-400"
-						/>
-					</div>
-					{/* Search button */}
 					<button
-						className="bg-[#18191A] hover:[#18191A]/50 text-white p-3 
-            flex items-center justify-center transition-colors rounded-r-2xl cursor-pointer"
+						aria-label="Select Person"
+						type="button"
+						className="flex items-center gap-2 flex-1 px-4 py-3 bg-white/90 cursor-pointer"
+					>
+						<UserRound className="w-6 h-6 text-[#18191A]" />
+						<p className="text-[#707577] font-light">0 Person</p>
+					</button>
+
+					{/* Search button */}
+					<Button
+						type="button"
+						className="h-full w-full bg-[#18191A] hover:[#18191A]/50 text-white p-3 
+            flex items-center rounded-l-none justify-center transition-colors rounded-r-2xl cursor-pointer"
 					>
 						<Search className="h-6 w-6" />
-					</button>
+					</Button>
 				</div>
 			</div>
 		</div>
