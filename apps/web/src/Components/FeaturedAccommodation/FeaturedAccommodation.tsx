5c135c411bf63c71d0746f9dec6e84eb727e8dc8
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-30 10:23:17 +0700

    feat: make feature accommodation section to be mobile responsive

--- apps/web/src/Components/FeaturedAccommodation/FeaturedAccommodation.tsx
+++ apps/web/src/Components/FeaturedAccommodation/FeaturedAccommodation.tsx
@@ -2,9 +2,12 @@ import AccommodationCard from "./AccommodationCard";
 
 const FeaturedAccommodation = () => {
 	return (
-		<section className="my-16 mx-32 flex flex-col gap-12 justify-center items-center">
+		<section className="my-16 mx-0 lg:mx-32 flex flex-col gap-12 justify-center items-center">
 			{/* Section Description */}
-			<h3 className="pr-8 text-left text-4xl lg:text-5xl text-[#18191A] font-medium leading-[150%]">
+			<h3
+				className="lg:pr-8 text-left text-3xl lg:text-5xl text-[#18191A] 
+        font-medium leading-[150%]"
+			>
 				StayMeran offers a curated selection of three unique accommodations,
 				each designed to provide an unforgettable stay in Merano.
 			</h3>
