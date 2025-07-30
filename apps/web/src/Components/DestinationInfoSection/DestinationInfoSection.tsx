2c6ab8ba15029e1861c2b0d6d05346e45d3e596e
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-30 10:54:26 +0700

    feat: update styling for DestinationInfoSection to improve layout and responsiveness

--- apps/web/src/Components/DestinationInfoSection/DestinationInfoSection.tsx
+++ apps/web/src/Components/DestinationInfoSection/DestinationInfoSection.tsx
@@ -1,22 +1,29 @@
 const DestinationInfoSection = () => {
 	return (
-		<section className="my-16 mx-0 lg:mx-32 flex flex-col gap-12 justify-center items-center">
-			<div className="w-full grid gap-5">
-				<h3
-					className="text-left text-3xl lg:text-5xl text-[#18191A] 
+		<section
+			className="w-screen relative left-1/2 right-1/2
+		 -mx-[50dvw] py-10 bg-[#F8FAFB]"
+		>
+			<div className="mx-4 lg:mx-36 flex flex-col gap-12 justify-center items-center">
+				<div className="w-full grid gap-5">
+					<h3
+						className="text-left text-3xl lg:text-5xl text-[#18191A] 
         font-medium leading-[150%]"
-				>
-					Why Stay in Merano?
-				</h3>
-				<p className="text-[#454849] text-lg lg:text-xl font-light leading-[200%]">
-					Where Nature, Culture, and Relaxation Meet Nestled in the heart of the
-					Alps, Merano is a captivating town known for its picturesque
-					landscapes, vibrant cultural scene, and renowned wellness experiences.
-					Whether you're exploring historic streets, indulging in world-class
-					cuisine, or unwinding in thermal spas, Merano has something for every
-					traveler.
-				</p>
+					>
+						Why Stay in Merano?
+					</h3>
+					<p className="text-[#454849] text-sm md:text-lg lg:text-xl font-light leading-[200%]">
+						Where Nature, Culture, and Relaxation Meet Nestled in the heart of
+						the Alps, Merano is a captivating town known for its picturesque
+						landscapes, vibrant cultural scene, and renowned wellness
+						experiences. Whether you're exploring historic streets, indulging in
+						world-class cuisine, or unwinding in thermal spas, Merano has
+						something for every traveler.
+					</p>
+				</div>
 			</div>
+
+			{/* <CarousalGallery /> */}
 		</section>
 	);
 };
