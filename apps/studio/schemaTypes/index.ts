13d42f3b1e6218034f8989c495c2e9897851062a
syahrilhanla <syahrilhanla5@gmail.com>
2025-08-01 00:12:07 +0700

    feat: add sectionHeaderSchema for testimonials and update schemaTypes

--- apps/studio/schemaTypes/index.ts
+++ apps/studio/schemaTypes/index.ts
@@ -1,9 +1,9 @@
 import { accommodationSchema } from "./accommodation";
 import { gallerySchema } from "./gallery";
 import { heroSchema } from "./hero";
-import { testimonialSchema } from "./testimonial";
+import { sectionHeaderSchema as testimonialHeaderSchema, testimonialSchema } from "./testimonial";
 
 export const schemaTypes = [
-  heroSchema, accommodationSchema, gallerySchema, testimonialSchema
+  heroSchema, accommodationSchema, gallerySchema, testimonialSchema, testimonialHeaderSchema
   // Add other schema types here as needed
 ]
--- apps/studio/schemaTypes/testimonial.ts
+++ apps/studio/schemaTypes/testimonial.ts
@@ -14,6 +14,76 @@ export const testimonialSchema = defineType({
   ],
 });
 
+export const sectionHeaderSchema = defineType({
+  name: 'testimonialSectionHeader',
+  title: 'Testimonial Section Header',
+  type: 'document',
+  fields: [
+    {
+      name: 'en_title',
+      title: 'English Title',
+      type: 'string',
+      options: {
+        documentInternationalization: {
+          exclude: true,
+        },
+      },
+    },
+    {
+      name: 'it_title',
+      title: 'Italian Title',
+      type: 'string',
+      options: {
+        documentInternationalization: {
+          exclude: true,
+        },
+      },
+    },
+    {
+      name: 'de_title',
+      title: 'German Title',
+      type: 'string',
+      options: {
+        documentInternationalization: {
+          exclude: true,
+        },
+      },
+    },
+    {
+      name: 'en_description',
+      title: 'English Description',
+      type: 'text',
+      options: {
+        documentInternationalization: {
+          exclude: true,
+        },
+      },
+    },
+    {
+      name: 'it_description',
+      title: 'Italian Description',
+      type: 'text',
+      options: {
+        documentInternationalization: {
+          exclude: true,
+        },
+      },
+    },
+    {
+      name: 'de_description',
+      title: 'German Description',
+      type: 'text',
+      options: {
+        documentInternationalization: {
+          exclude: true,
+        },
+      },
+    },
+  ]
+}
+);
+
 export const schemaTypes = [
   testimonialSchema,
+  sectionHeaderSchema,
 ];
