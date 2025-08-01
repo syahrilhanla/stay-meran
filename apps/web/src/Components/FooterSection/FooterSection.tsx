de24329ed58c42320c4c5df705e8bc53c31aa4eb
syahrilhanla <syahrilhanla5@gmail.com>
2025-08-01 22:37:35 +0700

    feat: add motion animations to FooterContacts, FooterLinks, and FooterSection components

--- apps/web/src/Components/FooterSection/FooterSection.tsx
+++ apps/web/src/Components/FooterSection/FooterSection.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { motion } from "motion/react";
+
 import LocaleSelector from "../Navbar/LocaleSelector";
 import FooterContacts from "./FooterContacts";
 import FooterLegalDropdown from "./FooterLegalDropdown";
@@ -5,7 +9,13 @@ import FooterLinks from "./FooterLinks";
 
 const FooterSection = () => {
 	return (
-		<footer className="mt-8 lg:mt-24 xl:mt-30 bg-[#18191A] box-border min-h-[30dvh] duration-300 transition-all">
+		<motion.footer
+			className="mt-8 lg:mt-24 xl:mt-30 bg-[#18191A] box-border min-h-[30dvh] duration-300 transition-all"
+			initial={{ opacity: 0, scaleY: 0, y: -100 }}
+			whileInView={{ opacity: 1, scaleY: 1, y: 0 }}
+			transition={{ duration: 0.2, ease: "anticipate" }}
+			viewport={{ once: true, amount: 0.1 }}
+		>
 			<div className="flex flex-col gap-4 lg:gap-8 px-4 md:px-24 lg:px-30 xl:px-36 py-4 lg:py-10 xl:py-20">
 				<FooterContacts />
 				<FooterLinks />
@@ -22,7 +32,7 @@ const FooterSection = () => {
 					</span>
 				</div>
 			</div>
-		</footer>
+		</motion.footer>
 	);
 };
 
